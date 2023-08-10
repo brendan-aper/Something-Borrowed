9768e89ff65e86fbae2edc0c2ad4d4d40fec6cda
NoahWillis <noahwillis123@gmail.com>
2023-08-09 18:53:13 -0700

    Created routes

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+
+const userRouter = require('./routes/users');
+app.use('/users', userRouter);
+
 app.listen(PORT, () => {
   console.log(`server listening at PORT:${PORT}`)
 })
