b29df18e08bb8e2f38a64dad02d0bb42fe036962
Merel Jacobs <merel.burleigh@gmail.com>
2023-08-09 20:41:38 -0700

    working on seeds

---
+++ controllers/html/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const pageRoutes = require('./pages')
+
+router.use('/' , pageRoutes)
+
+module.exports = router;
--- seeds/user-seeds.js
+++
@@ -1,7 +0,0 @@
-[{
-    "first_name": "Merel",
-    "last_name": "Jacobs",
-    "email": "mimibanini@gmail.com",
-    "password": "helloTest123",
-    "location": "Portland"
-}]
