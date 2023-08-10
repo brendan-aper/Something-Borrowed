68533e63c654097fba56d7fffb3d38dacc98a74e
Michie <contact@michiewillman.com>
2023-08-09 17:45:14 -0700

    Added session setup to server.js

--- server.js
+++ server.js
@@ -17,6 +17,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-  console.log(`Server is listening at PORT: ${PORT}`);
+// Set up cookie session
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+// Use session
+app.use(session(sess));
+
+// Use routes
+app.use(routes);
+
+// Run server after sequelize sets up database
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Server now listening on ${PORT}...`));
 });
