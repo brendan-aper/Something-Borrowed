1407dc1a9d1b9631758055a3d39b837c9ded0294
Michie <contact@michiewillman.com>
2023-08-09 17:51:48 -0700

    Import Sequelize & sessions to server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const express = require('express');
 const expbs = require('express-handlebars');
+const routes = require("./controllers");
 
+const sequelize = require("./config/connection");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
