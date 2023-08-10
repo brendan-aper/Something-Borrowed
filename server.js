c4f025dbdc61ee0b5ec97ffc59195f2508a2acc9
Brendan Aper <brendanaper@gmail.com>
2023-08-09 20:40:04 -0700

    fixed conflict merge

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session')
+const session = require('express-session');
 const expbs = require('express-handlebars');
-const routes = require('./controllers')
-const sequelize = require('./config/connection')
-
-const sequelize = require("./config/connection");
-const session = require("express-session");
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
