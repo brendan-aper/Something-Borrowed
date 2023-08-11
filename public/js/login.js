102ca53cbca2cdf4253c9ef328431ddf9149c591
Michie <contact@michiewillman.com>
2023-08-10 20:57:04 -0700

    Add login btn handler to login.js in public

---
+++ public/js/login.js
@@ -0,0 +1,20 @@
+async function loginHandler(event) {
+  event.preventDefault();
+  const userEmail = document.getElementById("login-email").value();
+  const userPass = document.getElementById("login-password").value();
+
+  const response = await fetch("/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email: userEmail,
+      password: userPass,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("Cannot log in. Please check your email and password and try again.");
+  }
+}
