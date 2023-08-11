fa052738139303e753ceee4520421e9b0427397e
Michie <contact@michiewillman.com>
2023-08-10 21:02:58 -0700

    Added logout function handler

---
+++ public/js/logout.js
@@ -0,0 +1,12 @@
+async function logoutHandler() {
+  const response = await fetch('/api/user/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Failed to log out');
+  }
+}
