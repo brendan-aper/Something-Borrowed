babeb03a10601f6ba8d78703594da1cab234ea1f
Michie <contact@michiewillman.com>
2023-08-12 15:52:32 -0700

    revert login.js to what Merel pushed

--- public/js/login.js
+++ public/js/login.js
@@ -1,30 +1,31 @@
-async function loginHandler(event) {
+const loginBtn = document.querySelector("#loginBtn");
+
+loginBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  const userEmail = document.getElementById("login-email").value();
-  const userPass = document.getElementById("login-password").value();
 
-    let email = document.querySelector('#login-email').value;
-    let password = document.querySelector('#login-password').value;
-    let location = document.querySelector('#login-location').value;
-    let name = document.querySelector('#login-name').value;
+  let email = document.querySelector("#login-email").value;
+  let password = document.querySelector("#login-password").value;
+  let location = document.querySelector("#login-location").value;
+  let name = document.querySelector("#login-name").value;
 
-    let userData = {
-        first_name: name,
-        email: email,
-        password: password,
-        location: location
-    }
+  let userData = {
+    first_name: name,
+    email: email,
+    password: password,
+    location: location,
+  };
 
-    console.log(userData);
-    
-        fetch('/api/user', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(userData)
-        }).then(window.location.href = '/')
-        .catch((err) => {
-            console.error(err); 
-        });
-};
+  console.log(userData);
+
+  fetch("/api/user", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((window.location.href = "/"))
+    .catch((err) => {
+      console.error(err);
+    });
+});
