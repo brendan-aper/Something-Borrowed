c57b59dee4d6ba3606f041930a65e93405833a5f
Michie <contact@michiewillman.com>
2023-08-12 21:55:50 -0700

    Change click to submit

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,30 +1,30 @@
-const submitBtn = document.querySelector('#submitBtn')
+const submitBtn = document.querySelector("#submitBtn");
 
-submitBtn.addEventListener('click', async (event) => {
-    event.preventDefault();
+submitBtn.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-    let email = document.querySelector('#signup-email').value;
-    let password = document.querySelector('#signup-password').value;
-    let location = document.querySelector('#signup-location').value;
-    let name = document.querySelector('#signup-name').value;
+  let email = document.querySelector("#signup-email").value;
+  let password = document.querySelector("#signup-password").value;
+  let location = document.querySelector("#signup-location").value;
+  let name = document.querySelector("#signup-name").value;
 
-    let userData = {
-        first_name: name,
-        email: email,
-        password: password,
-        location: location
-    }
+  let userData = {
+    first_name: name,
+    email: email,
+    password: password,
+    location: location,
+  };
 
-    try {
-        // First, send the POST request to /api/user
-        await fetch('/api/user', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(userData)
-        }).then(window.location.href = '/')
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    // First, send the POST request to /api/user
+    await fetch("/api/user", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    }).then((window.location.href = "/"));
+  } catch (err) {
+    console.error(err);
+  }
 });
