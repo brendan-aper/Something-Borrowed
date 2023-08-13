ae244d21d69d39ca49751d69639afa88824ebc29
Michie <contact@michiewillman.com>
2023-08-12 19:58:46 -0700

    Add authorize to item-routes

--- controllers/api/item-routes.js
+++ controllers/api/item-routes.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const { Item } = require('../../models');
+const express = require("express");
+const { Item } = require("../../models");
 const router = express.Router();
+const authorize = require("../../utils/auth");
 
 // GET all item
-router.get('/', async (req, res) => {
-    const allItems= await Item.findAll();
-    console.log("found");
+router.get("/", async (req, res) => {
+  const allItems = await Item.findAll();
+  console.log("found");
   res.json(allItems);
 });
 
 // GET a single item by ID
-router.get('/:id', async (req, res) => {
-    const singleItem = await Item.findByPk(req.params.id)
-    res.json(singleItem)
+router.get("/:id", async (req, res) => {
+  const singleItem = await Item.findByPk(req.params.id);
+  res.json(singleItem);
 });
 
 // POST a new item
-router.post('/', (req, res) => {
+router.post("/", authorize, (req, res) => {
   const newItem = req.body;
   newItem.id = Item.length + 1;
   Item.push(newItem);
@@ -24,36 +25,42 @@ router.post('/', (req, res) => {
 });
 
 // DELETE a item
-router.delete('/:id', async (req, res) => {
-    const finditem = await Item.destroy({ 
-        where: {id: req.params.id}});
-    console.log(`Item Deleted`);
-    res.json(findItem)
-})
+router.delete("/:id", authorize, async (req, res) => {
+  const finditem = await Item.destroy({
+    where: { id: req.params.id },
+  });
+  console.log(`Item Deleted`);
+  res.json(findItem);
+});
 
 // Updating a item
 // update post =  successful
-router.put('/:id', async (req, res) => {
-    try {
-    const updateItem = await Item.update({
-      title: req.body.title,
-      description: req.body.description,
-      price: req.body.price,
-      location: req.body.location
-    }, {
-      where: {
-        id: req.params.id
+router.put("/:id", authorize, async (req, res) => {
+  try {
+    const updateItem = await Item.update(
+      {
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        location: req.body.location,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
       }
-    })
-    console.log('updated');
-    res.json('updated item')
+    );
+    console.log("updated");
+    res.json("updated item");
   } catch (err) {
-    if (err.name === 'SequelizeUniqueConstraintError') {
-      return res.status(400).json({ error: 'Not enough information' });
+    if (err.name === "SequelizeUniqueConstraintError") {
+      return res.status(400).json({ error: "Not enough information" });
     }
     console.error(err);
-    return res.status(500).json({ error: 'An error occurred while creating a post.' });
-  }})
-
+    return res
+      .status(500)
+      .json({ error: "An error occurred while creating a post." });
+  }
+});
 
 module.exports = router;
