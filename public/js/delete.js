e0106828b4e4c440281dce63f5aecd71d580055b
Michie <contact@michiewillman.com>
2023-08-15 23:39:13 -0700

    Delete button on item is working now

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,8 +1,7 @@
 const deleteBtn = document.querySelector(".delete-btn");
-const itemId = document.querySelector('input[name="item-id"]').value;
 
 const deleteItem = async function () {
-  const itemId = document.querySelector('input[name="post-id"]').value;
+  const itemId = document.querySelector('input[name="item-id"]').value;
 
   await fetch(`/api/item/${itemId}`, {
     method: "DELETE",
